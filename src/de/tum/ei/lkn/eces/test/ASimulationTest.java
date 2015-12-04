966d2023e006b4835e5338759ce8bc9efddcc520
kwang90 <k.wang@tum.de>
2015-12-04 16:54:02 +0100

    Extended SF test setup with bugs

--- src/de/tum/ei/lkn/eces/test/ASimulationTest.java
+++ src/de/tum/ei/lkn/eces/test/ASimulationTest.java
@@ -6,6 +6,8 @@ import java.util.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.tum.ei.lkn.eces.dclc_routing.ExtendedSFAlgorithm;
+import de.tum.ei.lkn.eces.dclc_routing.SFAlgorithm;
 import de.tum.ei.lkn.eces.dclc_routing.datamodel.EdgePath;
 import de.tum.ei.lkn.eces.dclc_routing.datamodel.SDpare;
 import de.tum.ei.lkn.eces.framework.Controller;
@@ -13,9 +15,13 @@ import de.tum.ei.lkn.eces.framework.Entity;
 import de.tum.ei.lkn.eces.framework.Mapper;
 import de.tum.ei.lkn.eces.framework.MapperManager;
 import de.tum.ei.lkn.eces.framework.exceptions.ComponentLocationException;
+import de.tum.ei.lkn.eces.generaldijkstra.GeneralDijkstra;
+import de.tum.ei.lkn.eces.genmst.GenMST;
 import de.tum.ei.lkn.eces.graphsystem.GraphSystem;
+import de.tum.ei.lkn.eces.graphsystem.components.Edge;
 import de.tum.ei.lkn.eces.graphsystem.components.Node;
 import de.tum.ei.lkn.eces.networkcalculus.NCSystem;
+import de.tum.ei.lkn.eces.networkcalculus.components.NCCostFunction;
 import de.tum.ei.lkn.eces.networkcalculus.genetic.RoutingAlgorithmSettings;
 import de.tum.ei.lkn.eces.networkcalculus.genetic.RoutingAlgorithmSettings.RoutingAlgorithm;
 import de.tum.ei.lkn.eces.networking.NetworkingSystem;
@@ -28,20 +34,28 @@ import de.tum.ei.lkn.eces.topologies.settings.TopologyRingSettings;
 import de.tum.ei.lkn.simulation.TrafficSettings;
 
 public class ASimulationTest {
-	
-	Controller controller;
-	GraphSystem m_GraphSystem;
-	NetworkingSystem m_NetSys;
-	MapperManager mm;
-	NCSystem m_NCSystem;
-	
-	TopologyRingSettings m_TopoRingSetting;
-	RoutingAlgorithmSettings m_RASetting;
-	NetworkTopologyInterface m_Topology;
-	Vector<Node> m_SendingNodes;
-	Vector<Node> m_ReceivingNodes;
-	Mapper<EdgePath> edgePathMapper;
-	Vector<Entity[][]> entities;
+	//Framework
+	private Controller controller;
+	private GraphSystem m_GraphSystem;
+	private NetworkingSystem m_NetSys;
+	private MapperManager mm;
+	private NCSystem m_NCSystem;
+	//Topology
+	private TopologyRingSettings m_TopoRingSetting;
+	private RoutingAlgorithmSettings m_RASetting;
+	private NetworkTopologyInterface m_Topology;
+	private Vector<Node> m_SendingNodes;
+	private Vector<Node> m_ReceivingNodes;
+	private Vector<Entity[][]> entities;
+	//Mapper
+	private Mapper<EdgePath> edgePathMapper;
+	private Mapper<NCCostFunction> m_MapperNcData;
+	private Mapper<SDpare> m_MapperSdPare;	
+	//For SF Routing
+	private GeneralDijkstra genDijkLC;
+	private GeneralDijkstra genDijkLD;
+	private GenMST mstLC;
+	private GenMST mstLD;
 
 	@Before
 	public void setUp(){
@@ -51,27 +65,67 @@ public class ASimulationTest {
 		mm = new MapperManager(controller);
 		
 		edgePathMapper = new Mapper<EdgePath>(EdgePath.class);
+		m_MapperNcData = new Mapper<NCCostFunction>(NCCostFunction.class);
+		m_MapperSdPare = new Mapper<SDpare>(SDpare.class);
 		edgePathMapper.setController(controller);
+		m_MapperNcData.setController(controller);
+		m_MapperSdPare.setController(controller);
+
+		Mapper.initThreadlocal();
 
 		m_RASetting = new RoutingAlgorithmSettings();
-		m_RASetting.setRoutingAlgorithm(RoutingAlgorithm.BelmanFord);
+		m_RASetting.setRoutingAlgorithm(RoutingAlgorithm.Extended_SF);
 		m_NCSystem = new NCSystem(controller, m_GraphSystem, m_NetSys, m_RASetting, false);
-		
-		Mapper.initThreadlocal();
-		m_NCSystem.initRoutingAlgorithm(null);
-		mm.process();
 		
 		m_TopoRingSetting = new TopologyRingSettings();
 		m_TopoRingSetting.setRingSize(2);
 		m_TopoRingSetting.setBranchLength(2);
 		m_Topology = new OneRingFunnel(controller, m_NetSys, m_TopoRingSetting);
+		m_Topology.initGraph();
+		
+		Mapper.initThreadlocal();
+		m_NCSystem.initRoutingAlgorithm(m_Topology.getQGraph());
+		mm.process();
+		
+		//Settings for SF and Extended_SF
+		if(m_RASetting.getRoutingAlgorithm() == RoutingAlgorithm.SF_DCLC 
+			|| m_RASetting.getRoutingAlgorithm() == RoutingAlgorithm.Extended_SF ){
+			genDijkLC = new GeneralDijkstra(controller, m_Topology.getQGraph()) {
+				@Override
+				public double getEdgeCost(Edge nxtEdge) {
+					// TODO Auto-generated method stub
+					return m_MapperNcData.get_optimistic(nxtEdge.getEntity()).getCosts(controller.generateEntity(), null);
+				}
+			};
+			
+			genDijkLD = new GeneralDijkstra(controller, m_Topology.getQGraph()) {
+				@Override
+				public double getEdgeCost(Edge nxtEdge) {
+					// TODO Auto-generated method stub
+					return m_MapperNcData.get_optimistic(nxtEdge.getEntity()).getDelay(controller.generateEntity(), null);
+				}
+			};
+			genDijkLC.clearMemo();
+			genDijkLD.clearMemo();
+			mstLC = new GenMST(controller, genDijkLC);
+			mstLD = new GenMST(controller, genDijkLD);
+		}
+		
 		m_Topology.initTopology();
 		m_SendingNodes = m_Topology.getNodesAllowedToSend();
 		m_ReceivingNodes = m_Topology.getNodesAllowedToReceive();
-
+		
 		Mapper.initThreadlocal();
 		entities = getEntireEntitySet(controller, m_SendingNodes, m_ReceivingNodes);
 		mm.process();
+		
+		if(m_RASetting.getRoutingAlgorithm() == RoutingAlgorithm.SF_DCLC){
+			((SFAlgorithm<NCCostFunction>)(m_NCSystem.getAlgorithm())).preSF(controller, mstLC, mstLD);
+		}
+		
+		if(m_RASetting.getRoutingAlgorithm() == RoutingAlgorithm.Extended_SF){
+			((ExtendedSFAlgorithm<NCCostFunction>)(m_NCSystem.getAlgorithm())).preLDRun(controller, mstLD);
+		}
 	}
 	
 	@Test
@@ -82,6 +136,13 @@ public class ASimulationTest {
 		do{
 			Entity myFlow = entities.get(entities.size()-1)[0][rand.nextInt(3)];
 			Mapper.initThreadlocal();
+			Node src = m_MapperSdPare.get_optimistic(myFlow).getSource();
+			Node dest = m_MapperSdPare.get_optimistic(myFlow).getDestination();
+			System.out.println("src: " + src.getIdentifier() + "	--> dest : " + dest.getIdentifier());
+			//For ExtendedSF pre-run
+			if(m_RASetting.getRoutingAlgorithm() == RoutingAlgorithm.Extended_SF){
+				((ExtendedSFAlgorithm<NCCostFunction>)(m_NCSystem.getAlgorithm())).preLCRun(controller, mstLC, dest);
+			}
 			_pathFound = m_NCSystem.ncRequest(myFlow);
 			mm.process();
 			//EdgePath path = edgePathMapper.get_wait(myFlow);
